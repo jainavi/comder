d8e4bfcfe40689057273a50516f48fb6395523c0
Avi Jain <jainavi1408@gmail.com>
2022-11-28 11:43:09 +0530

    some changes

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const fs = require("node:fs");
 const path = require("node:path");
+const http = require("node:http");
 
 const { Client, Collection, GatewayIntentBits, Events } = require("discord.js");
-const { token } = require("./config.json");
 const mongoose = require("mongoose");
 
 const updateDatabase = require("./utilityFunctions/updateDatabase");
 const leetCodeStatsLive = require("./utilityFunctions/leetcodeStatsLive");
 const { send } = require("./utilityFunctions/messageSend");
+const keepAlive = require("./server");
 
 const client = new Client({
   intents: [
@@ -50,11 +51,14 @@ for (const file of eventFiles) {
   if (event.once) {
     client.once(event.name, (...args) => {
       event.execute(...args);
+
       send(
         client.channels.cache.get("1041717419460280341"),
         "Comdeerr Bot is Now Watching All COMDEERRSSSS :smirk_cat:!!"
       ).catch((err) => console.log(err));
+
       leetCodeStatsLive(client).catch((err) => console.log(err));
+
       setInterval(async () => {
         await updateDatabase().catch((err) => console.log(err));
         console.log("All User Database Updated");
@@ -67,10 +71,11 @@ for (const file of eventFiles) {
 
 mongoose
   .connect(
-    "mongodb+srv://backend:z2UhvyLhMkmn23s1@cluster0.p5unwig.mongodb.net/comder?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.p5unwig.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`
   )
   .then((result) => {
     console.log("Connectd to DataBase!");
-    client.login(token);
+    keepAlive();
+    client.login(process.env.TOKEN);
   })
   .catch((err) => console.log(err));
